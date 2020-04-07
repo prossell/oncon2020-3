d4192f3815ff7ec95f771fcecf0b0a6f30ad5b77
tussi5969 <sakura.0304.0226@gmail.com>
2020-04-08 03:44:02 +0900

    english #vision

--- components/organisms/SecVision.vue
+++ components/organisms/SecVision.vue
@@ -5,23 +5,23 @@
       .main_wrapper
         .idea
           IdeaSvg.idea_svg
-          .idea_txt 休暇中の学生にコロナに負けないビジコンを
+          .idea_txt {{ $t('vision.idea') }}
         .comment
           .comment_img
             img.comment_img_img(src="~/assets/images/yokoyama_kazuki.png")
             .comment_who(v-if="$mq === 'sm'")
-              .comment_who--post 運営代表
-              .comment_who--name 横山 和輝
+              .comment_who--post {{ $t('vision.post') }}
+              .comment_who--name {{ $t('vision.name') }}
           .comment_wrapper
             .comment_who(v-if="$mq !== 'sm'")
-              .comment_who--post 運営代表
-              .comment_who--name 横山 和輝
+              .comment_who--post {{ $t('vision.post') }}
+              .comment_who--name {{ $t('vision.name') }}
             .comment_txt
-              p 今、世界では新型コロナウイルス感染症 (COVID-19) が流行し、経済に大きな影響が出ています。外出自粛の中、学校もバイトも休みでなにをやればいいか分からないって思っている学生も多いのではないでしょうか。コロナが流行している今の社会だからこそ「アフターコロナ」「ウィズコロナ」について考えませんか？
-              p “Online Intern Contest 2020-2" では、完全オンラインで全国の学生と協力してコロナ対策に取り組めます！
-              p 就活イベントの多くが中止になって困っている21卒の学生さんや夏季インターンが開催されるか不安な22卒の学生さん大歓迎です！
-              p また第二回となる今回は高校生の参加もOKです！
-              p 前回のオンコンに参加していただいた学生も、今まで学外活動に取り組んだことのない学生も、社会のピンチを自分のチャンスに変えましょう！
+              p {{ $t('vision.comment_1') }}
+              p {{ $t('vision.comment_2') }}
+              p {{ $t('vision.comment_3') }}
+              p {{ $t('vision.comment_4') }}
+              p {{ $t('vision.comment_5') }}
         //- .matching
         //-   .matching_thinkings
         //-     .matching_thinkings_title: span 学生の<br>思い

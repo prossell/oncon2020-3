79ea48796e803d0062be068ac019ca713c647c8a
tussi5969 <sakura.0304.0226@gmail.com>
2020-04-08 12:20:28 +0900

    english #threefeatures

--- components/organisms/SecThreeFeatures.vue
+++ components/organisms/SecThreeFeatures.vue
@@ -6,9 +6,9 @@
         .feature_content
           .feature_topic(v-for="(feature,feature_id) in features")
             OneFeature(
-              :title="feature.title"
+              :title="'feature.' + feature + '.title'"
               :logo="feature_id"
-              :discription="feature.discription"
+              :discription="'feature.' + feature + '.discription'"
             )
 </template>
 <script>
@@ -23,32 +23,7 @@ export default {
   },
   data() {
     return {
-      features: [
-        {
-          title: '自宅でインターン',
-          discription: [
-            '地方学生でも大丈夫。',
-            '全てのプロセスがオンラインで完結！',
-            '最終発表もビデオ通話にて。'
-          ]
-        },
-        {
-          title: '企業とのつながり',
-          discription: [
-            '企業の方と繋がるチャンス。',
-            '積極的に会話していこう!',
-            'ヘッドハンティングの可能性も!'
-          ]
-        },
-        {
-          title: 'プロセス評価',
-          discription: [
-            '結果だけが全てじゃない。',
-            '活動プロセスを企業側へ共有して、',
-            '総合的な評価をもらおう!'
-          ]
-        }
-      ]
+      features: ['online', 'link', 'process']
     }
   }
 }
